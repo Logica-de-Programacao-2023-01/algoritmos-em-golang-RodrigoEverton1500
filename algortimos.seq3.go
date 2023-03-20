8b1e348bb6b8de32df0978899729e35fae6b08fd
Rodrigo Everton Carvalho de Abreu <rodrigo.everton@dinfor.uniceub>
2023-03-20 10:05:22 -0300

    resolvidos

--- algortimos.seq3.go
+++ algortimos.seq3.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	var num1 int64
-
+	var peso float64
+	var altura float64
+	fmt.Println("Digite o seu peso:")
+	fmt.Scan(&peso)
+	fmt.Println("Digite a sua altura")
+	fmt.Scan(&altura)
+	fmt.Println("Seu IMC é:", peso/altura)
 }
